82957cc186351de0339b6b6abc7f6c9d0cbb64ca
Andrew Schoewe <andrew@schoewe.me>
2018-05-09 15:13:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a very simple nodejs application that streams the current time in the response, every second.
+When the request comes in for each client, the first chunk of data sent back in the response is 1024 bytes of random data (happnes to be zeros).  This is because some browsers have a quirk that they don't rrender the data froma streamed/chunked response until they receive 1KB of data.  The amount varies by browser.  After this initial chunk is sent, the current time is pushed every second.
---
+++ stream-time.js
@@ -0,0 +1,36 @@
+var Stream = require('stream');
+var stream = new Stream();
+ 
+http = require('http');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    // some browsers only render the first browser response after 1KB of data
+    var randomStr = "";
+    for(var i = 0; i < 1024; i++) {
+        randomStr += "0";
+    }
+
+    stream.pipe = function(dest) {
+        dest.write(randomStr);
+        return dest;
+    };
+    stream.pipe(res);
+
+    // stream the time every second
+    setInterval(function(){
+        var currentTime = new Date();
+        var currentTimeStr =
+            currentTime.getHours()
+            + ':' +
+            currentTime.getMinutes()
+            + ':' +
+            currentTime.getSeconds() + "<br/>";
+                                               
+            stream.pipe = function(dest) {
+                dest.write(currentTimeStr);
+                return dest;
+            };
+            stream.pipe(res);
+    },1000);
+}).listen(8080);
